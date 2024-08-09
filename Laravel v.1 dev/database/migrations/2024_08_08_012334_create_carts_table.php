33fae475e852740a73f18780aeee4def980ecf7d
renaldikristian <renaldi.kristian@ti.ukdw.ac.id>
2024-08-09 14:51:03 +0700

    Update Foreign Key Table Carts

--- Laravel v.1 dev/database/migrations/2024_08_08_012334_create_carts_table.php
+++ Laravel v.1 dev/database/migrations/2024_08_08_012334_create_carts_table.php
@@ -12,8 +12,8 @@ return new class extends Migration {
     {
         Schema::create('carts', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->foreignId('user_id')->constrained('users')->onUpdate('cascade')->onDelete('cascade');
-            $table->foreignId('product_id')->constrained('products')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('product_id')->constrained('products');
             $table->integer('quantity');
             $table->timestamps();
         });
